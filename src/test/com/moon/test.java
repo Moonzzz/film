ccc7dad164aa3c145bf1e7fe2c75b89943f75e6e
pz13035085017@gmail.com <pz13035085017@gmail.com>
2018-12-29 15:47:32 +0800

    重构spring mvc配置文件

--- src/test/com/moon/test.java
+++ src/test/com/moon/test.java
@@ -12,7 +12,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import java.util.List;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"classpath:spring_mvc.xml","classpath:appContext.xml"})
+@ContextConfiguration(locations = {"classpath*:spring/spring_*.xml"})
 public class test {
     @Autowired
     SqlSessionFactory sqlSessionFactory;
